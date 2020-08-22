2b0dd63742d9f190d1dc087a19443e67c08410c9
jjz <390651928@qq.com>
2020-08-22 20:22:52 +0800

    添加vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <vuetify-test></vuetify-test>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import VuetifyTest from './VuetifyTest.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // eslint-disable-next-line vue/no-unused-components
+    VuetifyTest
   }
 }
 </script>
---
+++ src/views/VuetifyTest.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+  export default {
+    name: "VuetifyTest"
+  }
+</script>
+
+<style scoped>
+
+</style>
